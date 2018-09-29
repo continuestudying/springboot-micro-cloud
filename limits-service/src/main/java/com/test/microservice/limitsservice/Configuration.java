8ea9177dbcaebd86b4363d0a81e8c32277ea6217
continuestudying <continuestudying@gmail.com>
2018-09-29 11:46:57 +0700

    Create Spring Boot project naming limits-service

---
+++ limits-service/src/main/java/com/test/microservice/limitsservice/Configuration.java
@@ -0,0 +1,27 @@
+package com.test.microservice.limitsservice;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("limits-service")
+public class Configuration {
+	
+	private String minimum;
+	private String maximum;
+	
+	public String getMinimum() {
+		return minimum;
+	}
+	public void setMinimum(String minimum) {
+		this.minimum = minimum;
+	}
+	public String getMaximum() {
+		return maximum;
+	}
+	public void setMaximum(String maximum) {
+		this.maximum = maximum;
+	}
+	
+	
+}
