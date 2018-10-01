08957154ef4e619f88147064d15fa5bc437ed732
continuestudying <continuestudying@gmail.com>
2018-10-01 20:31:50 +0700

    Invoking currency-exchange-service from currency-conversation-service using RestTemplate

--- currency-conversation-service/src/main/java/com/test/microservice/currencyconversationservice/CurrencyConversationController.java
+++ currency-conversation-service/src/main/java/com/test/microservice/currencyconversationservice/CurrencyConversationController.java
@@ -4,9 +4,11 @@ import java.math.BigDecimal;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CurrencyConversationController {
@@ -19,14 +21,22 @@ public class CurrencyConversationController {
 			@PathVariable String from, 
 			@PathVariable String to, 
 			@PathVariable BigDecimal quantity) {
-		CurrencyConversationBean bean = new CurrencyConversationBean();
-		bean.setId("1001");
-		bean.setFrom(from);
-		bean.setTo(to);		
-		bean.setQuantity(quantity);
-		bean.setConversationMultiple(new BigDecimal(65));
+//		CurrencyConversationBean bean = new CurrencyConversationBean();
+//		bean.setId("1001");
+//		bean.setFrom(from);
+//		bean.setTo(to);		
+//		bean.setQuantity(quantity);
+//		bean.setConversationMultiple(new BigDecimal(65));
+//		bean.setTotalCalculatedAmount(quantity.multiply(bean.getConversationMultiple()));
+//		bean.setPort(env.getProperty("local.server.port"));
+		
+		RestTemplate restTemplate = new RestTemplate();
+		String url = "http://localhost:8000/currency-exchange/from/" + from + "/to/" + to ;
+		ResponseEntity<CurrencyConversationBean> response = restTemplate.getForEntity(url, CurrencyConversationBean.class);
+		CurrencyConversationBean bean = response.getBody();
 		bean.setTotalCalculatedAmount(quantity.multiply(bean.getConversationMultiple()));
 		bean.setPort(env.getProperty("local.server.port"));
+		
 		return bean;
 		
 	}
