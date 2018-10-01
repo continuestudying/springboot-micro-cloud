62f1edb816c784d512a402c3fa112ae33593c03a
continuestudying <continuestudying@gmail.com>
2018-10-01 21:14:30 +0700

    Invoking currency-exchange-service from currency-conversation-service using Feign Client

--- currency-conversation-service/src/main/java/com/test/microservice/currencyconversationservice/CurrencyConversationController.java
+++ currency-conversation-service/src/main/java/com/test/microservice/currencyconversationservice/CurrencyConversationController.java
@@ -3,6 +3,7 @@ package com.test.microservice.currencyconversationservice;
 import java.math.BigDecimal;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,11 +11,16 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import feign.Feign;
+
 @RestController
 public class CurrencyConversationController {
 	
 	@Autowired
-	private Environment env;	
+	private Environment env;
+	
+	@Autowired
+	private CurrencyExchangeServiceProxy feignProxy;
 	
 	@GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConversationBean getCurrencyConversation(
@@ -39,6 +45,17 @@ public class CurrencyConversationController {
 		
 		return bean;
 		
+	}
+	
+	@GetMapping("/currency-converter-feign/from/{from}/to/{to}/quantity/{quantity}")
+	public CurrencyConversationBean getCurrencyConversationViaFeignProxy(
+			@PathVariable String from,
+			@PathVariable String to,
+			@PathVariable BigDecimal quantity) {
+		CurrencyConversationBean bean = feignProxy.getCurrencyConversation(from, to);
+		bean.setTotalCalculatedAmount(quantity.multiply(bean.getConversationMultiple()));
+		bean.setPort(env.getProperty("local.server.port"));
+		return bean;
 	}
 	
 }
