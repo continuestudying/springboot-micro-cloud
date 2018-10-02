4f2e901e41ab25552ccf22777012b91ed4287835
continuestudying <continuestudying@gmail.com>
2018-10-02 20:31:58 +0700

    Set up Eureka Server & connect micro-services

--- currency-conversation-service/src/main/java/com/test/microservice/currencyconversationservice/CurrencyConversationServiceApplication.java
+++ currency-conversation-service/src/main/java/com/test/microservice/currencyconversationservice/CurrencyConversationServiceApplication.java
@@ -2,10 +2,12 @@ package com.test.microservice.currencyconversationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 public class CurrencyConversationServiceApplication {
 
 	public static void main(String[] args) {
